7b719d6cc25e8640f67e62b9fae16df6709e62ba
njokuchisomjoseph@gmail.com <njokuchisomjoseph@gmail.com>
2025-02-07 15:59:26 +0100

    Fix: edited getLevels helper  to work with new db

--- helpers/getLevels.js
+++ helpers/getLevels.js
@@ -3,8 +3,11 @@ const { Level } = require("../models");
 module.exports = async () => {
   try {
     const levels = await Level.findAll();
-    return levels.length > 0 ? levels : [];
+    return levels.length > 0
+      ? levels.sort((a, b) => a.name.localeCompare(b.name))
+      : [];
   } catch (error) {
+    console.log("ERROR GETTING LEVELS");
     console.log(error);
     return [];
   }
